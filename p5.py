3cf6815479dda9d6486b064f8f89ae521d749305
Manas Mahale <36999652+Manas02@users.noreply.github.com>
2020-11-02 16:14:03 +0530

    removed os.mkdir("./Project/library") as its useless

--- p5.py
+++ p5.py
@@ -6,7 +6,6 @@ import argparse
 def stuff(option="p5"):
     if option == "p5":
         os.mkdir("./Project")
-        os.mkdir("./Project/library")
         f = open("./Project/index.html","w")
         html_p5= ("<!DOCTYPE html>\n  <head>\n    <meta charset=\"utf-8\">\n    <title>P5.js</title>\n    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/p5.min.js\"></script>\n    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/addons/p5.dom.min.js\"></script>\n    <script src=\"sketch.js\"></script>\n  </head>\n</html>")
         f.write(html_p5)
@@ -16,7 +15,6 @@ def stuff(option="p5"):
         i.close()
     elif option == "ml5":
         os.mkdir("./Project")
-        os.mkdir("./Project/library")
         f = open("./Project/index.html","w")
         html_ml5= ("<!DOCTYPE html>\n  <head>\n    <meta charset=\"utf-8\">\n    <title>ml5.js</title>\n    <script src=\"https://unpkg.com/ml5@0.4.3/dist/ml5.min.js\"></script>\n    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/p5.min.js\"></script>\n    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/addons/p5.dom.min.js\"></script>\n    <script src=\"sketch.js\"></script>\n  </head>\n</html>")
         f.write(html_ml5)
